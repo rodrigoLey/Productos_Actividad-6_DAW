7cd6a0e8ca31f1cc9173e6978e100544c96c027b
rodrigoLey <rodrigo.ley_@hotmail.com>
2023-03-02 18:20:04 -0500

    Vistas productos

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,23 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+/* Vistas productos */
+/*
+Route::get('/productos', function () {
+    return view('productos.index');
+});
+
+Route::get('/productos/create', function () {
+    return view('productos.create');
+});
+
+Route::get('/productos/edit', function () {
+    return view('productos.edit');
+});
+*/
+
+/*Rutas de vista mediante Controlador*/
+
 Route::get('/productos/edit',[ProductoController::class,'edit']);
 Route::get('/productos/create',[ProductoController::class,'create']);
 Route::get('/productos',[ProductoController::class,'index']);
